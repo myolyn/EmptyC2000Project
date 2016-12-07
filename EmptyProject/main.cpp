c1ece6614bbabbc929661f01032bdc1dd7f7a80e
JohnTaylor <john.taylor@myolyn.com>
2016-12-07 16:25:33 -0500

    first commit

---
+++ EmptyProject/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * MyocycleHome.c
+ *
+ *  Created on: Aug 15, 2016
+ *      Author: JohnTaylor
+ */
+
+
+
+#include "HardwareAbstractionLayer/hal.h"
+#include "Util/memCopy.h"
+#include "Util/constants.h"
+#include <stdbool.h>
+
+/*
+ * Test Variables
+ */
+
+/*
+ * end Test Variables
+ */
+
+int main(void)
+
+{
+
+
+
+	while(true)
+	{
+
+	}
+
+}
